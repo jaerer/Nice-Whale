0e11f4f908de411d116418bc99c170c79ca9a950
joellerennick <j2rennic@uwaterloo.ca>
2020-09-18 20:46:47 -0700

    Initial Commit

---
+++ Nice WhaleTests/Nice_WhaleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nice_WhaleTests.swift
+//  Nice WhaleTests
+//
+//  Created by Joëlle Rennick on 2020-09-18.
+//  Copyright © 2020 Joëlle Rennick. All rights reserved.
+//
+
+import XCTest
+@testable import Nice_Whale
+
+class Nice_WhaleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
